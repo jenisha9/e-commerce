8360a34ef8d80e06ccad72a13d17c2583dba9290
jenisha9 <njenisha78@gmail.com>
2024-04-29 15:39:17 +0545

    feat: main body

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import PageHeader from './components/PageHeader.vue'
+import MainBody from './components/MainBody.vue'
 </script>
 
 <template>
 <PageHeader/>
+<MainBody/>
 </template>
 
 <style scoped>
