cb09728a739d476e03d8ef697f1af301e02ee3d9
jenisha9 <njenisha78@gmail.com>
2024-04-29 15:40:21 +0545

    feat: integrate font awesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
-// import './style.css'
+import { createApp } from 'vue';
 import './assets/tailwind.css'
-import App from './App.vue'
+import App from './App.vue';
+import VueSlickCarousel from 'vue-slick-carousel';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(fas, far, fab);
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.component(VueSlickCarousel.name, VueSlickCarousel)
 
 createApp(App).mount('#app')
