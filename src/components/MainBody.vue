9fc8c83c173d8aa26eb03c530df27f3106208d63
jenisha9 <njenisha78@gmail.com>
2024-04-29 15:38:00 +0545

    feat: add body elements

---
+++ src/components/MainBody.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-cover bg-center h-screen" style="background-image: url('/slider01.webp')">
+    <div class=" ml-20 max-w-xl p-10">
+    <h1 class="mt-20 text-7xl font-bold mb-5">
+        Every dog has its every day!
+     </h1>
+     <p class="mt-8 mb-8 text-xl">
+        We've all heard people who say that pets are animals, as though somehow they're in our lives
+        people have likely never owned a loyal dog !
+     </p>
+     <div class= "mb-8 text-xl"> Starting rate : <span class="text-myorange"> $70.00 </span> </div>
+     <button class="bg-myorange rounded-xl text-white p-4">
+        SHOP ONLINE
+     </button>
+    </div>
+  </div>
+</template>
